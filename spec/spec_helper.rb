7faadae3ec1d3c6fb1ad88385c739ce708956c20
John Drago <jdrago.999@gmail.coml>
2016-04-17 23:57:49 -0700

    Remove byebug so things work well on the docker container we're using.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ SimpleCov.minimum_coverage 100
 
 require 'rspec'
 require 'bundler/setup'
-require 'byebug'
 lib = File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'active_support/core_ext/hash'
